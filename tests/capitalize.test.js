08020830fa469ecd1b498fc2053ef0bf19167d05
Kirill Pastukhov <Pastukhov475@icloud.com>
2021-03-04 19:19:41 +0300

    add test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -4,5 +4,9 @@ import { capitalize } from '../src/capitalize.js';
 
 assert(capitalize('hello') === 'Hello');
 assert(capitalize('') === '');
+test('capital', () => {
+expect(capitalize('hello')).toEqual('Hello');
+expect(capitalize('hi')).toEqual('Hi');
+});
 
 console.log('Все тесты пройдены!');
